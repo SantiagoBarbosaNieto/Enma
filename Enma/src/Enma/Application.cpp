e19f14c11c832e2c7fe6f067c181c285a4f7c083
Santiago Barbosa <santibar15@hotmail.com>
2022-06-18 14:41:45 -0500

    Added abstraction for Vertex Buffer Layouts

--- Enma/src/Enma/Application.cpp
+++ Enma/src/Enma/Application.cpp
@@ -14,6 +14,28 @@ namespace Enma {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case ShaderDataType::Float:    return GL_FLOAT;
+			case ShaderDataType::Float2:   return GL_FLOAT;
+			case ShaderDataType::Float3:   return GL_FLOAT;
+			case ShaderDataType::Float4:   return GL_FLOAT;
+			case ShaderDataType::Mat3:     return GL_FLOAT;
+			case ShaderDataType::Mat4:     return GL_FLOAT;
+			case ShaderDataType::Int:      return GL_INT;
+			case ShaderDataType::Int2:     return GL_INT;
+			case ShaderDataType::Int3:     return GL_INT;
+			case ShaderDataType::Int4:     return GL_INT;
+			case ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		EM_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
+
 	Application::Application() {
 		EM_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
@@ -28,18 +50,39 @@ namespace Enma {
 		glBindVertexArray(m_VertexArray);
 
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
 		};
 
 
 		// Vertex Buffer
 		m_VertexBuffer.reset( VertexBuffer::Create(vertices, sizeof(vertices)) );
 		
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, 
+				element.GetComponentCount(), 
+				ShaderDataTypeToOpenGLBaseType(element.Type), 
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
 
 		unsigned int indices[3] = { 0,1,2 };
 
@@ -51,12 +94,15 @@ namespace Enma {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
+			out vec4 v_Color;
 			out vec3 v_Position;
 			
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position,1.0);
 				
 			}
@@ -68,11 +114,13 @@ namespace Enma {
 			
 			layout(location = 0) out vec4 color;
 			
+			in vec4 v_Color;
 			in vec3 v_Position;
 
 			void main()
 			{
-				color = vec4(v_Position+0.5,1.0);
+				color = vec4(v_Position*0.5 + 0.5, 1.0);
+				color = v_Color;
 				
 			}
 		)";
