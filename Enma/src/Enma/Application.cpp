a1485c2e77c089c429399f0f5f56297cbac3afd9
Santiago Barbosa <santibar15@hotmail.com>
2022-06-24 12:03:55 -0500

    Added texture blending

--- Enma/src/Enma/Application.cpp
+++ Enma/src/Enma/Application.cpp
@@ -25,6 +25,8 @@ namespace Enma {
 		m_Window = Scope<Window>(Window::Create());
 		m_Window->SetEventCallback(EM_BIND_EVENT_FN(Application::OnEvent));
 
+		Renderer::Init();
+
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
 
--- Enma/src/Enma/Renderer/RendererAPI.h
+++ Enma/src/Enma/Renderer/RendererAPI.h
@@ -16,6 +16,8 @@ namespace Enma
 			OpenGL = 1
 		};
 	public:
+		virtual void Init() = 0;
+
 		virtual void SetClearColor(const glm::vec4 color) = 0;
 		virtual void Clear() = 0;
 
