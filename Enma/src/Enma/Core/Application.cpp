3a1ac8d94095143817c2e75ef880b8a2333b4e3a
Santiago Barbosa <santibar15@hotmail.com>
2022-12-13 13:08:39 -0500

    Fixed layer->OnAttach() placement

--- Enma/src/Enma/Core/Application.cpp
+++ Enma/src/Enma/Core/Application.cpp
@@ -39,11 +39,13 @@ namespace Enma {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* overlay)
 	{
 		m_LayerStack.PushOverlay(overlay);
+		overlay->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e)
