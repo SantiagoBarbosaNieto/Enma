50b638d80383777ac8cc9218edc170d9c209a72a
Santiago Barbosa <santibar15@hotmail.com>
2022-12-14 23:29:00 -0500

    Fix last commit - Log.cpp

--- Enma/src/Enma/Core/Log.cpp
+++ Enma/src/Enma/Core/Log.cpp
@@ -1,5 +1,6 @@
 #include "empch.h"
 #include "Enma/Core/Log.h"
+
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include <spdlog/sinks/basic_file_sink.h>
 
@@ -8,8 +9,8 @@ namespace Enma {
 	Ref<spdlog::logger> Log::s_CoreLogger;
 	Ref<spdlog::logger> Log::s_ClientLogger;
 
-	void Log::Init() 
-	{	
+	void Log::Init()
+	{
 		std::vector<spdlog::sink_ptr> logSinks;
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Enma.log", true));
@@ -17,15 +18,15 @@ namespace Enma {
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
+		s_CoreLogger = std::make_shared<spdlog::logger>("ENMA", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
+		s_CoreLogger->set_level(spdlog::level::trace);
 		s_CoreLogger->flush_on(spdlog::level::trace);
+
 		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
 		s_ClientLogger->flush_on(spdlog::level::trace);
-		s_CoreLogger->set_level(spdlog::level::trace);
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
-		s_ClientLogger->set_level(spdlog::level::trace);
-
 	}
 
 }
